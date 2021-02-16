53cbeefe7de0c58b2cb370c7a147c28348af822b
Ewan Woodcock <ew440@kent.ac.uk>
2021-02-16 13:42:56 +0000

    I made the changes!

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@ function answerQuery(query) {
   query = query.toLowerCase();
   if (query.indexOf("rain") != -1 || query.indexOf("sun") != -1 || query.indexOf("weather") != -1)
     return "I do not care too much about weather, I'm locked inside a data center.";
+  else if (query.indexOf("football") != -1 || query.indexOf("footie") != -1)
+    return "I'm indifferent to football as I have no legs.";
   return "Sorry Dave, I cannot do that."
 }
 
